e2158e0593808f95230ddf45dc4086f0501b49f7
OSCAR RIOS <oscr.rss@gmail.com>
2018-11-23 17:26:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// add solution here
+musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr'];
+
+instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums']
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < 4; i++) {
+      array.push(`${musicians[i]} plays ${instruments[i]}`);
+    }
+  return array;
+}
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+function johnLennonFacts(facts) {
+  while (facts) 
+  return facts + '!!!!!'; 
+}
